d6c7e8c11e583ebdc8db8f99e6c4548dc54b0436
Anjalee01 <anjaleeramwani01@gmail.com>
2024-07-09 22:43:14 +0500

    first commit

---
+++ Assignments/assignnment01.py
@@ -0,0 +1,22 @@
+# Assignment_01
+
+# 1.Write a Python program to print "Hello, World!".
+# 2.Declare a variable name and assign your name to it. Print the value of the variable.
+# 3.Declare variables using camelCase, snake_case, and PascalCase. Assign values to them and print each variable.
+# 4.Write a Python program that declares a variable and prints its memory address using the id() function.
+# 5.Write a Python program that takes user input and prints it.
+# 6.Write a Python program to display the data types of different variables (e.g., integer, float, string).
+# 7.Write a Python program that takes two numbers as input and performs addition and subtraction. Display the results.
+# 8.Write a Python program to demonstrate operator precedence. Use addition, subtraction, multiplication, and division in a single expression and show the result. (5 + 3 * 2 - 4 / 2)
+# 9.Write a  Python program to calculate the area of a rectangle based on user input for its length and width. 
+# Hint:  Take input from the user to enter the length of the rectangle.
+# Take input from the user to enter the width of the rectangle.
+# Area=length×width
+# 10. Write a Python program that performs the following steps:
+# ●Define a string vowels containing all lowercase vowels ('aeiou').
+# ●Take input from the user to enter a letter (lowercase). Check if the entered letter is 'a' using membership operators (in or not in). Print True if the entered letter is 'a', otherwise, print False.
+
+
+
+# Submission Date: The submission date of the assignment is June 23, 2024.
+# Submission link: https://forms.gle/YtEMQfteMDmzRRgs8
