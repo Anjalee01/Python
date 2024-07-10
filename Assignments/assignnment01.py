aedc368cb90a4bb6663808fc02d1b065d500a627
Anjalee01 <anjaleeramwani01@gmail.com>
2024-07-10 12:18:17 +0500

    first commit

--- Assignments/assignnment01.py
+++ Assignments/assignnment01.py
@@ -1,20 +1,73 @@
 # Assignment_01
 
 # 1.Write a Python program to print "Hello, World!".
+
+print("Hello")
 # 2.Declare a variable name and assign your name to it. Print the value of the variable.
+# name =  "anjalee"
+# print(name)
 # 3.Declare variables using camelCase, snake_case, and PascalCase. Assign values to them and print each variable.
+# firstName = "anjalee"
+# Last_name= "ramwani"
+# FatherName = "Moti Lal"
+# print(firstName, Last_name, FatherName)
 # 4.Write a Python program that declares a variable and prints its memory address using the id() function.
+
+a = 3
+print(id(a))
+
+
 # 5.Write a Python program that takes user input and prints it.
+
+# name = input("Enter your name")
+# print(name)
+
 # 6.Write a Python program to display the data types of different variables (e.g., integer, float, string).
+
+a = 3
+b = 3.5
+name = "anjalee"
+print(type(a), type(b), type(name))
+
+
 # 7.Write a Python program that takes two numbers as input and performs addition and subtraction. Display the results.
+
+# a = 2
+# b = 1
+# print(a-b)
+# print(a+b)
+
 # 8.Write a Python program to demonstrate operator precedence. Use addition, subtraction, multiplication, and division in a single expression and show the result. (5 + 3 * 2 - 4 / 2)
+ 
+# a = 5
+# b = 4
+# c = 3
+# d = 2
+
+# print (a + c * d - b  / d)
+
+
 # 9.Write a  Python program to calculate the area of a rectangle based on user input for its length and width. 
+
+# length= int(input("Enter length"))
+# width = int(input("Enter width"))
+
+# area = (length * width)
+# print (area)
+
+
 # Hint:  Take input from the user to enter the length of the rectangle.
 # Take input from the user to enter the width of the rectangle.
 # Area=length×width
 # 10. Write a Python program that performs the following steps:
 # ●Define a string vowels containing all lowercase vowels ('aeiou').
+
 # ●Take input from the user to enter a letter (lowercase). Check if the entered letter is 'a' using membership operators (in or not in). Print True if the entered letter is 'a', otherwise, print False.
+# vowels = 'aeiou'
+
+# letter = input(" Enter letter ")
+
+# print (letter in 'a')
 
 
 
