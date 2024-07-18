894f071648fed3cab1953f7c1119d1f48347c67f
Anjalee01 <anjaleeramwani01@gmail.com>
2024-07-18 12:50:56 +0500

    first commit

---
+++ Assignments/assignment04.py
@@ -0,0 +1,35 @@
+# Assignment_04
+# Write a program to check whether a number is divisible by 7 or not.
+
+# num = int(input("Enter number "))
+# if num%7==0:
+#     print( '$num' "is divisable by 7" )
+# else: print("not divisable")
+
+
+# Find the length of the text python and convert the value to float and convert it to string.
+
+# text = "python"
+# print(len(text))
+
+
+
+# Write a program to check whether a year is a leap year or not.
+
+# Write a program to check whether a number is prime or not.
+
+num = int(input("Enter Number "))
+if (num%num == 0 and num%1==0):
+    print("num is prime")
+else: print(num is not prime)
+# Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+#   #
+#   ##
+#   ###
+#   ####
+#   #####
+#   ######
+#   #######
+# Use and operator to check if 'on' is found in both 'python' and 'dragon'
+# Submission Date: The submission date of the assignment is July 01, 2024
+# Submission link: https://forms.gle/wEHHkhtVWnTUw1U96
